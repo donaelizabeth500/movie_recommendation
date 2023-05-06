42108bda0e423ea2800812950ce04dd19b610d10
donaelizabeth500 <37398139+donaelizabeth500@users.noreply.github.com>
2023-05-06 19:38:07 +0100

    app.py
    update

--- solution/app.py
+++ solution/app.py
@@ -5,16 +5,19 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 import streamlit as st
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import MinMaxScaler
 
 
 st.set_page_config(page_title='Movie Recommendation System',page_icon="memo",layout="wide")
 st.title('Movie Recommendation System')
 
 
-st.sidebar.image('/app/movie_recommendation/solution/img.png')
+st.sidebar.image('img.png')
 
-def get_recommendation(movie):
-    df = pd.read_csv('/app/movie_recommendation/solution/movie_dataset.csv')
+def get_recommendation_method1(movie):
+    df = pd.read_csv('movie_dataset.csv')
 
     ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
     ratings['num of ratings'] = pd.DataFrame(df.groupby('title')['rating'].count())
@@ -34,13 +37,70 @@ def get_recommendation(movie):
     recommended_movies_df = recommended_movies.iloc[1:11,:]
     return recommended_movies_df
 
-df = pd.read_csv('/app/movie_recommendation/solution/movie_dataset.csv')
+
+# Function to recommend movies based on user input
+def recommend_movies(user_id, num_recommendations):
+    # Get the cosine similarity scores for the user
+    user_cosine_sim = cosine_sim[user_id-1]
+
+    # Get the indices of the top recommended movies
+    top_movies_idx = user_cosine_sim.argsort()[::-1][:num_recommendations]
+
+    # Get the movie IDs of the top recommended movies
+    recommendations = list(ratings_matrix.columns[top_movies_idx])
+
+    return recommendations
+
+def get_recommendation_method2(user_id,num_recommendations):
+    df = pd.read_csv('movie_dataset.csv')
+
+    # Pivot the data to create a user-item matrix
+    ratings_matrix = df.pivot(index='user_id', columns='item_id', values='rating').fillna(0)
+
+    # Normalize the ratings matrix
+    scaler = MinMaxScaler()
+    ratings_matrix_scaled = scaler.fit_transform(ratings_matrix)
+
+    # Create a dictionary mapping item IDs to their titles
+    item_titles = dict(zip(df['item_id'], df['title']))
+
+    # Factorize the ratings matrix using truncated SVD
+    svd = TruncatedSVD(n_components=50, random_state=42)
+    ratings_matrix_svd = svd.fit_transform(ratings_matrix_scaled)
+
+    # Calculate the cosine similarity matrix
+    cosine_sim = cosine_similarity(ratings_matrix_svd)
+    
+    # Get the cosine similarity scores for the user
+    user_cosine_sim = cosine_sim[user_id-1]
+
+    # Get the indices of the top recommended movies
+    top_movies_idx = user_cosine_sim.argsort()[::-1][:num_recommendations]
+
+    # Get the movie IDs of the top recommended movies
+    recommendations = list(ratings_matrix.columns[top_movies_idx])
+    
+    recommended_movie = []
+    for i in recommendations:
+        recommended_movie.append(item_titles[i])
+        
+    recommended_movie = pd.DataFrame(recommended_movie,columns=['Recommended Movies'])
+
+    return recommended_movie
+
+df = pd.read_csv('movie_dataset.csv')
 movies_list = list(df['title'].unique())
 movie_title = st.sidebar.selectbox('Select a movie:', movies_list)
+user = st.sidebar.slider('User-ID',1,900,450)
+recommended = get_recommendation_method2(user,10)
 
-if st.sidebar.button('Get Recommendations'):
+if st.sidebar.button("Get Recommendations Based on Users"):
+    st.table(recommended)
+    
+if st.sidebar.button('Get Recommendations Based on Ratings'):
+    
     st.subheader('Top 10 Similar Movies:')
-    top_n_movies = get_recommendation(movie_title)
+    top_n_movies = get_recommendation_method1(movie_title)
     
     recommended_movies  = list(top_n_movies.index)
     recommended_movies_df = pd.DataFrame(recommended_movies,columns=['Recommended Movie'])
@@ -62,7 +122,7 @@ if st.sidebar.button('Get Recommendations'):
     sns.barplot(x='predicted_rating', y=top_n_movies.index, data=top_n_movies, palette='Oranges_d')
     ax2.set_xlabel('Predicted Rating')
     ax2.set_ylabel('Movie Title')
-    ax2.set_title('Top 10 Recommended Moviepivots for User')
+    ax2.set_title('Top 10 Recommended Movies for User')
     st.pyplot(fig2)
     
     # Plot a histogram of the ratings distribution for the recommended movies
@@ -83,4 +143,3 @@ if st.sidebar.button('Get Recommendations'):
     ax.set_ylabel('Movie Title')
     ax.set_title('Number of Ratings for Recommended Movies')
     st.pyplot(fig)
-    #end
